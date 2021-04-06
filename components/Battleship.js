d9b39d1046352d946c577895f86d45788688f73f
MikkeGF <n9pemi00students.oamk.fi>
2021-04-06 15:16:27 +0300

    testikoodia

--- components/Battleship.js
+++ components/Battleship.js
@@ -44,11 +44,12 @@ export default function Battleship() {
     const [board, setBoard] = useState([]);
     const [numbers, setNumbers] = useState([])
     const timerRef = useRef();
+    console.log(ships)
 
     // possible to select difficulty of game. No UI yet.
     const [size, setSize] = useState(5)
     const [clickcount, setClickCount] = useState(15)
-    
+
     const makeBoard = () => {
         let gameboard = [];
         gameboard = Array(size * size).fill(START)
